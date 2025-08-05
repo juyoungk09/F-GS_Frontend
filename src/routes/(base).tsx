ec6f42018768f5773ac233f3e6e72f3c3453d44c
juyoungk09 <juyoungk09@gmail.com>
2025-08-06 01:20:58 +0900

    feat:folder routing improved and fixed

--- src/routes/(base).tsx
+++ src/routes/(base).tsx
@@ -1,7 +1,7 @@
 import { RouteSectionProps } from "@solidjs/router";
-import Header from "~/components/Header";
-import Footer from "~/components/Footer";
-import Sidebar from "~/components/Sidebar";
+import Header from "~/components/layout/Header";
+import Footer from "~/components/layout/Footer";
+import Sidebar from "~/components/layout/Sidebar";
 
 
 export default function Layout(props: RouteSectionProps) {
