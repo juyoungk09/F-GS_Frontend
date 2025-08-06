922b70c2e3ed58548e938adfee4b463d55abd769
wlqrksmswnd <wlqrksmswnd@gmail.com>
2025-08-07 00:44:51 +0900

    refactor:Btn Redesign

--- src/routes/(auth)/user/login.tsx
+++ src/routes/(auth)/user/login.tsx
@@ -22,20 +22,21 @@ const Login = () => {
                 <div class="bg-[#1F1F1F] w-5/11 h-5/7 flex flex-col justify-center items-center gap-4 stroke-[#686868] rounded-xl border-[#686868] border-1">
                     <div class="flex flex-col gap-10">
                         <div class="flex flex-col gap-1">
-                            <div class="font-wanted-sans font-medium">이메일</div>
+                            <div class="font-medium text-good_gray">이메일</div>
                             <input class="p-2 bg-[#2F2F2F] border-[#585858] border-2 rounded-md w-70 h-8" type="text"
                             value={emailInput()} oninput={(e) => setEmailInput(e.currentTarget.value)}></input>
                         </div>
                         <div class="flex flex-col gap-1">
-                            <div class="font-wanted-sans font-medium">비밀번호</div>
+                            <div class="font-medium text-good_gray">비밀번호</div>
                             <input class="p-2 bg-[#2F2F2F] border-[#585858] border-2 rounded-md w-70 h-8" type="text"
                             value={passwordInput()} oninput={(e) => setPasswordInput(e.currentTarget.value)}></input>
                         </div>
-                        <button class="bg-primary_color_3 w-70 h-8 rounded-md" onclick={handleLogin}>로그인</button>
+                        <button class="bg-primary_color_3 text-[#111327] w-70 h-8 rounded-md hover:bg-primary_color_2/100 hover:text-[#111327]/50 transition-colors"
+                        onclick={handleLogin}>로그인</button>
                     </div>
                     <div class="flex gap-1">
-                        <span class="font-wanted-sans font-medium">계정이 필요한가요?</span>
-                        <A href="/user/signup" class="font-wanted-sans hover:underline font-bold">회원가입</A>
+                        <span class="font-medium text-good_gray">계정이 필요한가요?</span>
+                        <A href="/user/signup" class="hover:underline font-bold text-good_gray">회원가입</A>
                     </div>
                 </div>
             </div>
--- src/routes/(auth)/user/signup.tsx
+++ src/routes/(auth)/user/signup.tsx
@@ -10,7 +10,7 @@ const Signup = () => {
         setEmailInput('');
         setNicnameInput('');
         setPasswordInput('');
-        navigate("/")
+        navigate("/user/login")
     }
     return (
         <main class="bg-primary_color_4 w-screen h-screen text-white flex flex-col gap-19">
@@ -24,25 +24,26 @@ const Signup = () => {
                 <div class="bg-[#1F1F1F] w-5/11 h-5/7 flex flex-col justify-center items-center gap-4 stroke-[#686868] rounded-xl border-[#686868] border-1">
                     <div class="flex flex-col gap-6">
                         <div class="flex flex-col gap-1">
-                            <div class="font-wanted-sans font-medium">이메일</div>
+                            <div class="font-medium text-good_gray">이메일</div>
                             <input class="p-2 bg-[#2F2F2F] border-[#585858] border-2 rounded-md w-70 h-8" type="text"
                             value={emailInput()} oninput={(e) => setEmailInput(e.currentTarget.value)}></input>
                         </div>
                         <div class="flex flex-col gap-1">
-                            <div class="font-wanted-sans font-medium">닉네임</div>
+                            <div class="font-medium text-good_gray">닉네임</div>
                             <input class="p-2 bg-[#2F2F2F] border-[#585858] border-2 rounded-md w-70 h-8" type="text"
                             value={nicknameInput()} oninput={(e) => setNicnameInput(e.currentTarget.value)}></input>
                         </div>
                         <div class="flex flex-col gap-1">
-                            <div class="font-wanted-sans font-medium">비밀번호</div>
+                            <div class="font-medium text-good_gray">비밀번호</div>
                             <input class="p-2 bg-[#2F2F2F] border-[#585858] border-2 rounded-md w-70 h-8" type="text"
                             value={passwordInput()} oninput={(e) => setPasswordInput(e.currentTarget.value)}></input>
                         </div>
-                        <button class="bg-primary_color_3 w-70 h-8 rounded-md" onclick={handleSignUp}>회원가입</button>
+                        <button class="bg-primary_color_3 text-[#111327] w-70 h-8 rounded-md  hover:bg-primary_color_2/100 hover:text-[#111327]/50 transition-colors "
+                        onclick={handleSignUp}>회원가입</button>
                     </div>
                     <div class="flex gap-1">
-                        <span class="font-wanted-sans font-medium">계정이 이미 있나요?</span>
-                        <A href="/user/login" class="font-wanted-sans hover:underline font-bold">로그인</A>
+                        <span class="font-medium text-good_gray">계정이 이미 있나요?</span>
+                        <A href="/user/login" class="hover:underline font-bold text-good_gray">로그인</A>
                     </div>
                 </div>
             </div>
